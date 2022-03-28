0360f2401320d6725501937d403e3d9763c18cf6
y2d2 <yuri.durodie@gmail.com>
2022-03-28 21:11:09 +0200

    improving documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'IDlab-Turtlebot'
+copyright = '2022, IMEC'
+author = 'Yuri Durodie'
 
 release = '0.1'
 version = '0.1.0'
